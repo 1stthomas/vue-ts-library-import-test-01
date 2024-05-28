89716614c53898cdcdc000c0e97295d1acc0cd16
1stthomas <thomas.pluess@students.ffhs.ch>
2024-05-28 18:56:32 +0200

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
-// import type { ButtonSimple } from '@types/vue-ts-library-test-01'
-// import { type ButtonSimple } from 'vue-ts-library-test-01'
 import { ButtonSimple } from 'vue-ts-library-test-01'
 import "vue-ts-library-test-01/style.css"
 </script>
 
 <template>
   <ButtonSimple text="Run the Tests" />
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
+  <ButtonSimple text="Run another Tests" />
+  <ButtonSimple text="Nix da" />
 </template>
 
 <style scoped>
